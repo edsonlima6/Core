3eaa33a2144d90a11f90093e6e495bb2805b0c8e
Edson <you@example.comedsonlima6@gmail.com>
2018-12-18 19:33:22 -0200

    JWT Identity

---
+++ NetDocsCore2_1/Model/ApiIdentity.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace NetDocsCore2_1.Model
+{
+    public class ApiIdentity
+    {
+
+    }
+
+    public class User
+    {
+        public string UserID { get; set; }
+        public string Password { get; set; }
+    }
+
+    public static class Roles
+    {
+        public const string ROLE_API_ALTURAS = "Acesso-APIAlturas";
+    }
+
+    public class TokenConfigurations
+    {
+        public string Audience { get; set; }
+        public string Issuer { get; set; }
+        public int Seconds { get; set; }
+    }
+
+    public class SigningConfigurations
+    {
+        public SecurityKey Key { get; }
+        public SigningCredentials SigningCredentials { get; }
+
+        public SigningConfigurations()
+        {
+            using (var provider = new RSACryptoServiceProvider(2048))
+            {
+                Key = new RsaSecurityKey(provider.ExportParameters(true));
+            }
+
+            SigningCredentials = new SigningCredentials(
+                Key, SecurityAlgorithms.RsaSha256Signature);
+        }
+    }
+}
--- NetDocsCore2_1/Startup.cs
+++ NetDocsCore2_1/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using NetDocsCore2_1.Model;
+using Infra.Context;
+using Infra.CrossCutting2.Context;
+using Infra.CrossCutting2.Application;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 namespace NetDocsCore2_1
 {
@@ -26,6 +34,20 @@ namespace NetDocsCore2_1
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            //options =>
+            //    options.UseSqlServer(Configuration.GetConnectionString("BaseIdentity"))
+            services.AddDbContext<ContextDB>();
+
+            // Configurando o uso da classe de contexto para
+            // acesso às tabelas do ASP.NET Identity Core
+            // permitir a recuperação de seus objetos via injeção de
+            // dependências 
+            services.AddIdentity<ApplicationUser, IdentRole>()
+                .AddEntityFrameworkStores<ContextCrossDB>()
+                .AddDefaultTokenProviders();
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,5 +65,57 @@ namespace NetDocsCore2_1
             app.UseHttpsRedirection();
             app.UseMvc();
         }
+
+        private void SetupJWT(IServiceCollection services)
+        {
+            try
+            {
+                var signingConfigurations = new SigningConfigurations();
+                services.AddSingleton(signingConfigurations);
+
+                var tokenConfigurations = new TokenConfigurations();
+                new ConfigureFromConfigurationOptions<TokenConfigurations>(
+                    Configuration.GetSection("TokenConfigurations"))
+                        .Configure(tokenConfigurations);
+                services.AddSingleton(tokenConfigurations);
+
+                services.AddAuthentication(authOptions =>
+                {
+                    authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                }).AddJwtBearer(bearerOptions =>
+                {
+                    var paramsValidation = bearerOptions.TokenValidationParameters;
+                    paramsValidation.IssuerSigningKey = signingConfigurations.Key;
+                    paramsValidation.ValidAudience = tokenConfigurations.Audience;
+                    paramsValidation.ValidIssuer = tokenConfigurations.Issuer;
+
+                    // Valida a assinatura de um token recebido
+                    paramsValidation.ValidateIssuerSigningKey = true;
+
+                    // Verifica se um token recebido ainda é válido
+                    paramsValidation.ValidateLifetime = true;
+
+                    // Tempo de tolerância para a expiração de um token (utilizado
+                    // caso haja problemas de sincronismo de horário entre diferentes
+                    // computadores envolvidos no processo de comunicação)
+                    paramsValidation.ClockSkew = TimeSpan.Zero;
+                });
+
+                // Ativa o uso do token como forma de autorizar o acesso
+                // a recursos deste projeto
+                services.AddAuthorization(auth =>
+                {
+                    auth.AddPolicy("Bearer", new AuthorizationPolicyBuilder()
+                        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                        .RequireAuthenticatedUser().Build());
+                });
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
     }
 }
