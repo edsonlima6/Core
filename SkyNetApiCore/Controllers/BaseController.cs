1e80f0c704954545c957d61981747670a95146e4
Edson <edsonlima6@gmail.com>
2023-03-26 18:53:14 +0100

    WIP: add custom response method to base controller

--- SkyNetApiCore/Controllers/BaseController.cs
+++ SkyNetApiCore/Controllers/BaseController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace SkyNetApiCore.Controllers
@@ -11,7 +12,25 @@ namespace SkyNetApiCore.Controllers
     [ApiController]
     public class BaseController : ControllerBase
     {
+        public BaseController()
+        {
+            //Here I put the notification object
+        }
 
+        protected virtual IActionResult CustomResponse(object result = null, HttpStatusCode httpStatusCode = HttpStatusCode.OK)
+        {
+            return Ok(new
+            {
+                code = HttpStatusCode.OK,
+                data = result,   
+                errors = new List<string> { } //"Success", "Error"
+            });
+        }
+
+        protected void AddNotification()
+        {
+
+        }
        
 
     }
