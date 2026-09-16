01add9e9b496ea2c0f445b072fe8fae97bfe2753
edsonlima6 <edsonlima6@gmail.com>
2026-09-16 22:32:38 +0100

    [SkyNetApis]: Created one more microservice

--- Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © Sky Net")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Domain layer to use in different projects/solutions")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21d34c4c58ce39bddd59ad9c130a24295f516fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07188ea414dc481462b838aced30609ed6f2c4a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SkyNetRisen.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- UnitTest/obj/Debug/net10.0/UnitTest.AssemblyInfo.cs
+++ UnitTest/obj/Debug/net10.0/UnitTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21d34c4c58ce39bddd59ad9c130a24295f516fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07188ea414dc481462b838aced30609ed6f2c4a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
